13f6ebc2e255d790507bb1867dc2dc1a235b05aa
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-06-04 22:38:54 +0300

    Created view functions to generate data for hoods, business and posts.

--- neighbors/views.py
+++ neighbors/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from neighbors.models import Neighborhood
+from django.shortcuts import render, redirect, get_object_or_404
+from neighbors.models import Neighborhood, Business, Contact, Post
+from neighbors.forms import (AddBusinessForm, AddContactForm, 
+                             AddNeighborhoodForm, AddPostForm)
+from django.urls import reverse
+from django.http import Http404
 
 # Create your views here.
 
@@ -12,15 +16,12 @@ def home(request):
     # }
     
     title = "Neighbors"
+    posts = Post.objects.all()[:3]
+    hoods = Neighborhood.objects.all()[:3]
+    business = Business.objects.all()[:3]
     
     
-    
-    return render(request, 'neighbors/index.html', {"title":title})
-
-
-def create_neighhood(request):
-    pass
-
+    return render(request, 'neighbors/index.html', {"title":title, "posts":posts, "hoods":hoods, "business":business})
 
 
 def neighborhoods(request):
@@ -28,9 +29,36 @@ def neighborhoods(request):
     
     return render(request, 'neighbors/location.html', {"hoods":hoods})
 
+def create_neighhood(request):
+    current_user = request.user.profile
+    
+    form = AddNeighborhoodForm()
+    if request.method =='POST':
+        form = AddNeighborhoodForm(request.POST)
+        if form.is_valid():
+            hood = form.save(commit = False)
+            hood.profile = current_user
+            hood.save()
+            return redirect(reverse('accounts:view_profile'))
+        else:
+            form = AddNeighborhoodForm()
+            
+    return render(request, 'neighbors/add_hood.html',{"form":form})
+
+
+
+
 
 
 def neighborhood_detail(request, id):
     hoods = Neighborhood.objects.all()
     
     return render(request, 'neighbors/details.html', {"hoods":hoods})
+
+
+def posts(request):
+    posts = Post.objects.all()
+    
+    
+    
+    return render(request, 'neighbors/posts.html', {"posts":posts})
